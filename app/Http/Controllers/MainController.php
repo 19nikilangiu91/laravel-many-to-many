ffc1f5e4d698bfe621b38da66abcbf0859ae7845
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-14 19:32:01 +0100

    Update $code and $product

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// 20a) Recupero il Faker
+use Faker\Generator as Faker;
+
 // Importo il model "Category"
 use App\Models\Category;
+
+// Importo il model "Product"
+use App\Models\Product;
 
 class MainController extends Controller
 {
@@ -38,6 +44,13 @@ class MainController extends Controller
             'weight' => 'required|integer',
         ]);
 
+        // 20b) Dichiaro il $code per non causare rotture con il DB.
+        $code = rand(10000, 99999);
+        $data['code'] = $code;
         dd($data);
+
+        // 21) Creo un nuovo "Product"
+        $product = Product::make($data);
+        dd($product);
     }
 }
