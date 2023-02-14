52d9f0a4cb11c164e48230ced80af39f901d5865
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-14 17:29:02 +0100

    Created and Recalled ProductSeeder in DataSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,12 +15,14 @@ class DatabaseSeeder extends Seeder
 
     // 7) Richiamo il "CategorySeeder" nel DBSeeder
     // 9) Richiamo il "TypologySeeder" nel DBSeeder
+    // 13) Richiamo il "ProductSeeder" nel DBSeeder
     public function run()
     {
         $this->call([
 
             CategorySeeder::class,
             TypologySeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+// Importo i model "Category, Product, Typology"
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Typology;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+    // 12) Dal model "Product" richiamiamo la "factory" e tramite il foreach richiamiamo ogni "singolo Product ($p)"
+    public function run()
+    {
+        Product::factory()->count(100)->make()->each(function ($p) {
+
+            // Richiamiamo il model "Typology" in ordine casuale e andiamo a prendere il primo elemento casuale
+            $typology = Typology::inRandomOrder()->first();
+
+            // Richiamiamo "$p" alla funzione inversa nel Model "Product" e associamo una singola "Typology ($typology)"
+            $p->typology()->associate($typology);
+
+            // Salviamo il nuovo elemento
+            $p->save();
+
+            // 14) Andiamo a creare un prodotto casuale e andiamo ad associarla nella Tabella Ponte "category_pruduct" N a M
+            $categories = Category::inRandomOrder()->limit(rand(1, 5))->get();
+            $p->categories()->attach($categories);
+        });
+    }
+
+}
