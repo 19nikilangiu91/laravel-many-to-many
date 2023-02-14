4f2128f844ddcbdfdf8341241645785cc0361f17
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-14 18:21:09 +0100

    Added Comment

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -23,7 +23,7 @@ class ProductSeeder extends Seeder
     {
         Product::factory()->count(100)->make()->each(function ($p) {
 
-            // Richiamiamo il model "Typology" in ordine casuale e andiamo a prendere il primo elemento casuale
+            // Richiamiamo il model "Typology" in ordine casuale e andiamo a prendere il primo elemento casuale. FK
             $typology = Typology::inRandomOrder()->first();
 
             // Richiamiamo "$p" alla funzione inversa nel Model "Product" e associamo una singola "Typology ($typology)"
@@ -32,7 +32,7 @@ class ProductSeeder extends Seeder
             // Salviamo il nuovo elemento
             $p->save();
 
-            // 14) Andiamo a creare un prodotto casuale e andiamo ad associarla nella Tabella Ponte "category_pruduct" N a M
+            // 14) Andiamo a creare un prodotto casuale e andiamo ad associarla nella Tabella Ponte "category_pruduct". N a M
             $categories = Category::inRandomOrder()->limit(rand(1, 5))->get();
             $p->categories()->attach($categories);
         });
